e8df4c403d86126447bd4c7c4f4edadfb631e6c0
Vinu Joseph <vinujosef@gmail.com>
2021-05-22 23:29:46 +0300

    fix: update scheduler to run sample-task at 5am everydayy

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const axios = require('axios');
 const cron = require('node-cron');
 
 // Data for sending
-const data = JSON.stringify({"content":"Axios Test", "parent_id": +process.env.PARENT_ID}); // "+" before .env value is to keep the PARENT_ID as number. If "+" is not added, the PARENT_ID is taken as string
-console.log(data);
+const data = JSON.stringify({"content":"Axios Test2", "parent_id": +process.env.PARENT_ID}); // "+" before .env value is to keep the PARENT_ID as number. If "+" is not added, the PARENT_ID is taken as string
 
 // Config - method, url, headers, data
 const config = {
@@ -18,8 +17,8 @@ const config = {
   "data": data
 };
 
-// Add a new task every minute
-cron.schedule('* * * * *', function() {
+// Add a new task at 5:00 am every day
+cron.schedule('0 5 * * *', function() {
   console.log(Date.now());
   // Axios setup
   axios(config)
