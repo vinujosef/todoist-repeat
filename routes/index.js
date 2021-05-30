f1aac5ab165263b60071a69da1e2a012fcef9740
Vinu Joseph <vinujosef@gmail.com>
2021-05-31 01:48:09 +0300

    feat: update index route, add code to fire a cron-job every day

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,40 @@
-var express = require('express');
-var router = express.Router();
+require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const cron = require('node-cron');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // Data for sending
+  const data = JSON.stringify({"content":`Axios ${Date.now()}`, "parent_id": +process.env.PARENT_ID}); // "+" before .env value is to keep the PARENT_ID as number. If "+" is not added, the PARENT_ID is taken as string
+
+  // Config - method, url, headers, data
+  const config = {
+    "method": "post",
+    "url": process.env.URL,
+    "headers": {
+      "Content-Type": "application/json",
+      "Authorization": process.env.AUTHORIZATION
+    },
+    "data": data
+  };
+  console.log("Reached here => ");
+  // Add a new task at 5:00 am every day
+  // cron.schedule('0 5 * * *', function() {
+
+  // Add a new task every 3 hours
+  cron.schedule('0 */2 * * *', function() {
+    console.log("Reached 4: " + Date.now());
+    // Axios setup
+    axios(config)
+      .then((result) => {
+        console.log(JSON.stringify(result.data));
+      }).catch((err) => {
+        console.log(err.message);
+      });
+  });
+
   res.render('index', { title: 'Express' });
 });
 
